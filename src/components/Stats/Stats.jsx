287f68a8d80e0b7e48196111052708960162fa20
WebdeveloperNahid <omarfaruk.nahid.webdeveloper@gmail.com>
2026-03-31 17:18:18 +0600

    stats section  responsive so small dicise

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Stats = () => {
   return (
     <div className="mt-15 ">
-      <div className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] py-10 flex justify-center space-x-15">
+      <div className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] py-10 flex justify-center space-x-9">
         <div>
           <h1 className="text-2xl text-white font-semibold">50K+</h1>
           <p className="text-[#bebebe] text-[12px] pt-3 ">Active Users</p>
         </div>
-        <div className="border-x-2 px-20 text-white ">
+        <div className="border-x-2 px-13 text-white ">
           <h1 className="text-2xl text-white font-semibold">200+</h1>
           <p className="text-[#bebebe] text-[12px] pt-3 ">Premium Tools</p>
         </div>
